1f25a8b4cb32957e211634b69b114389b564b2cb
EricTRL <eric_arts@live.nl>
2019-07-16 15:34:54 +0200

    Issue #1: view and update membership info: Added disclaimer
    Added a disclaimer in urls_api.py and urls_frontend.py to signify that these are not yet used (nor applicable) in this branch.
    
    The (template) code was not removed as it'll be useful for other tasks (E.g. members being able to view and update their own information).

--- membership_file/urls_api.py
+++ membership_file/urls_api.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views_api as views
 
 urlpatterns = [
-    path('', views.MemberView.as_view(), name='members-api'),
-    path('<int:id>/', views.SpecificMemberView.as_view(), name='member-api'),
+    # The following URLs are not of use currently; administrators can do everything
+    # via the Django Admin Panel
+    #path('', views.MemberView.as_view(), name='members-api'),
+    #path('<int:id>/', views.SpecificMemberView.as_view(), name='member-api'),
 ]
--- membership_file/urls_frontend.py
+++ membership_file/urls_frontend.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views_frontend as views
 
 urlpatterns = [
-    path('', views.viewAllMembers, name='members-frontend'),
-    path('<int:id>/', views.viewSpecificMember, name='member-frontend'),
+    # The following URLs are not of use currently; users updating their own
+    # information is not within the scope of this branch
+    #path('', views.viewAllMembers, name='members-frontend'),
+    #path('<int:id>/', views.viewSpecificMember, name='member-frontend'),
 ]
