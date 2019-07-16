a19df60294cafb1046f47d1f349504e61ea3ad8a
EricTRL <eric_arts@live.nl>
2019-07-16 11:58:38 +0200

    Issue #1: View and Update membership info: admin panel
    last_updated_by of a member is now automatically set to the admin that last updated that member using Django's admin panel, and cannot be overridden by any admin.
    NB: This will also happen is a request is manually forged by an admin.
    
    Added read only variants of the MemberLog and MemberLogField models to the Django admin panel. Old logs cannot be deleted in the panel and new logs cannot be created there either.

--- membership_file/admin.py
+++ membership_file/admin.py
@@ -1,4 +1,49 @@
 from django.contrib import admin
-from .models import Member
+from .models import Member, MemberLog, MemberLogField
+from django.contrib import admin
 
-admin.site.register([Member])
+# Ensures that the last_updated_by field is also updated properly from the Django admin panel
+class MemberWithLog(admin.ModelAdmin):
+    # Show the date and user that last updated the member
+    readonly_fields = ['last_updated_by', 'last_updated_date']
+
+    # Override the admin panel's save method to automatically include the user that updated the member
+    def save_model(self, request, obj, form, change):
+        obj.last_updated_by = request.user
+        super().save_model(request, obj, form, change)
+
+# Prevents MemberLog creation, edting, or deletion in the Django Admin Panel
+class MemberLogReadOnly(admin.ModelAdmin):
+    # Show the date at which the information was updated as well
+    readonly_fields = ['date']
+
+    # Disable creation
+    def has_add_permission(self, request):
+        return False
+    
+    # Disable editing
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    # Disable deletion
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+# Prevents MemberLogField creation, edting, or deletion in the Django Admin Panel
+class MemberLogFieldReadOnly(admin.ModelAdmin):
+    # Disable creation
+    def has_add_permission(self, request):
+        return False
+    
+    # Disable editing
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    # Disable deletion
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+# Register the special models, making them show up in the Django admin panel
+admin.site.register(Member, MemberWithLog)
+admin.site.register(MemberLog, MemberLogReadOnly)
+admin.site.register(MemberLogField, MemberLogFieldReadOnly)
