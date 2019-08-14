1c402a3ba2fd9ce980a2c19741eb963686a56487
EricTRL <eric_arts@live.nl>
2019-08-14 14:42:56 +0200

    Issue #1: View and update membership info: Code Cleanup
    Removed unused code.

--- membership_file/urls_frontend.py
+++ membership_file/urls_frontend.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views_frontend as views
 
 urlpatterns = [
-    # The following URLs are not of use currently; users updating their own
-    # information is not within the scope of this branch
+    # The following URLs are not of use currently;
+    # TODO: Provide a way for members to view (and update) their own membership information
     #path('', views.viewAllMembers, name='members-frontend'),
     #path('<int:id>/', views.viewSpecificMember, name='member-frontend'),
 ]
--- membership_file/views_api.py
+++ membership_file/views_api.py
@@ -1,36 +1,3 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Member, MemberLog
-from .serializers import MemberSerializer
-from rest_framework import routers, viewsets
-from rest_framework.views import APIView, Response
-
-# @require_safe only accepts HTTP GET and HEAD requests
-from django.views.decorators.http import require_safe
 
 # Enable the auto-creation of logs
 from .auto_model_update import *
-
-
-# API calls made to /members/
-class MemberView(APIView):
-    def get(self, request, format=None):
-        #Convert model instance to a dictionary
-        serializer = MemberSerializer(Member.objects.all(), many=True)
-        return Response({"members": serializer.data})
-    def post(self, request, format=None):
-        pass
-
-
-# API calls made to /members/<int:id>/
-class SpecificMemberView(APIView):
-    def get(self, request, id, format=None):
-        pMember = get_object_or_404(Member, pk=id)
-        #Convert model instance to a dictionary
-        serializer = MemberSerializer(pMember)
-
-        return Response(serializer.data)
-    def put(self, request, id, format=None):
-        pass
-    
-    def delete(self, request, id, format=None):
-        pass
