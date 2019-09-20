304e5ad0a4db5a49b09992f5e739d6ecc3388935
HansNorg <52379922+HansNorg@users.noreply.github.com>
2019-09-20 16:37:41 +0200

    Issue #16: Adding the ability to add members to achievements.: Added first Testcase
    Added the first testcase: view all achievements

--- achievements/tests.py
+++ achievements/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .serializers import AchievementSerializer
+from .models import Achievement
 
 # Create your tests here.
+class TestCaseAchievementFrontEndViews(TestCase):
+    @classmethod
+
+    def setUp(self):
+        # Dit runt 1x per testcase (ervoor) binnen deze class
+        self.client = Client()
+
+    def test_case_view_all_achievements(self):
+         #dit is een testcase
+         testSerializer = {"achievements" : (AchievementSerializer(Achievement.objects.all(),many=True)).data}
+         response = self.client.get('/achievements/' , data=testSerializer, secure=True)
+
+         self.assertEqual(response.status_code, 200)
