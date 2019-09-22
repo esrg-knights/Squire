217f010de91f9e6e3b199f4574b5dfb8e2c91e9a
Hans Norg <h.g.norg@student.tue.nl>
2019-09-22 14:46:59 +0200

    Issue #16: Adding the ability to add members to achievements.: Added last frontend view testcases
    Added testcases for viewing specific categories, achievements and members

--- achievements/tests.py
+++ achievements/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase, Client
 from .serializers import AchievementSerializer, CategorySerializer
 from .models import Achievement, Category
+from membership_file.models import Member
+from membership_file.serializers import MemberSerializer
 
 # Create your tests here.
 class TestCaseAchievementFrontEndViews(TestCase):
@@ -10,49 +12,64 @@ class TestCaseAchievementFrontEndViews(TestCase):
         # This runs 1x per testcase (before) within this class
         self.client = Client()
 
-    def test_case_view_all_achievements(self):
-         #This is a TestCase
-         testSerializer = {"achievements" : (AchievementSerializer(Achievement.objects.all(),many=True)).data}
-         response = self.client.get('/achievements/' , data=testSerializer, secure=True)
-         self.assertEqual(response.status_code, 200)
+        # Make a Category
+        self.categoryData = {
+            "name": "ZG-Category",
+            "description": "Boardgames-description",
+        }
+        self.category = Category.objects.create(**self.categoryData)
 
-    # def test_case_view_specific_achievement(self):
-    #      testSerializer = (AchievementSerializer(Achievement.objects.all().first(),many=True)).data
-    #      response = self.client.get('/achievements/1' , data=testSerializer, secure=True)
-    #      self.assertEqual(response.status_code, 200)
+        # Save the model
+        Category.save(self.category)
+
+        # Make an Achievement
+        self.achievementData = {
+            "category": Category.objects.all().first(),
+            "name": "Achievement_name",
+            "description": "Achievement_description",
+        }
+        self.achievement = Achievement.objects.create(**self.achievementData)
+        Achievement.save(self.achievement)
+
+        # Make a Member
+        self.memberData = {
+            "first_name": "Fantasy",
+            "last_name": "Court",
+            "date_of_birth": "1970-01-01",
+            "email": "info@fantasycourt.nl",
+            "street": "Veld",
+            "house_number": "5",
+            "city": "Eindhoven",
+            "country": "The Netherlands",
+            "postal_code": "5612 AH",
+            "member_since": "1970-01-01",
+        }
+        self.member = Member.objects.create(**self.memberData)
+        Member.save(self.member)
 
     def test_case_view_all_categories(self):
          testSerializer = {"categories" : (CategorySerializer(Category.objects.all(),many=True)).data}
-         response = self.client.get('/achievements/categories/' , data=testSerializer, secure=True)
+         response = self.client.get('/achievements/categories/', data=testSerializer, secure=True)
          self.assertEqual(response.status_code, 200)
 
-    # def test_case_view_specific_category(self):
-    #      testSerializer = (CategorySerializer(Category.objects.all().first(),many=True)).data
-    #      response = self.client.get('/achievements/categories/1' , data=testSerializer, secure=True)
-    #
-    #      self.assertEqual(response.status_code, 200)
+    def test_case_view_specific_category(self):
+         testSerializer = (CategorySerializer(Category.objects.all().first())).data
+         response = self.client.get('/achievements/categories/1', data=testSerializer, follow=True, secure=True)
 
-# class TestCaseAchievementModels(TestCase):
-#     @classmethod
-#     #def setUpTestData(self):
-#         # dit runt in totaal maar 1x (voor alle tests binnen deze class)
-#
-#     def setUp(self):
-#         # Dit runt 1x per testcase (ervoor) binnen deze class
-#         #self.client = Client()
-#         self.categoryData = {
-#             "name": "ZG-Category",
-#             "description": "Boardgames-description",
-#         }
-#         self.category = Category.objects.create(**self.categoryData)
-#
-#         # Save the models
-#         Category.save(self.category)
-#
-#     def test_case_get_category_name(self):
-#          # dit is een testcase
-#          self.name = self.category.name
-#          self.assertIsNotNone(self.name)
-#          self.category.delete()
-#          # No category should exist if it's deleted
-#          self.assertIsNone(Category.objects.all().first())
+         self.assertEqual(response.status_code, 200)
+
+    def test_case_view_all_achievements(self):
+        #This is a TestCase
+        testSerializer = {"achievements" : (AchievementSerializer(Achievement.objects.all(),many=True)).data}
+        response = self.client.get('/achievements/', data=testSerializer, secure=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_case_view_specific_achievement(self):
+        testSerializer = (AchievementSerializer(Achievement.objects.all().first())).data
+        response = self.client.get('/achievements/1', data=testSerializer, follow=True, secure=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_case_view_specific_member(self):
+        #testSerializer = (MemberSerializer(Member.objects.all().first())).data
+        response = self.client.get('/achievements/members/1', data=self.memberData, follow=True, secure=True)
+        self.assertEqual(response.status_code, 200)
