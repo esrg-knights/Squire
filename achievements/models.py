310b37123c692d9fb262d1e20c5cde3f061acacf
EricTRL <eric_arts@live.nl>
2020-02-25 20:20:17 +0100

    Issue #32: View Earned Achievements:
    Updated the achievement model:
    - Made the achievements earnable by Users instead of Members (to remove the dependency between the membership file and the achievement system)
    - Ensured that achievements are not deleted when their category is deleted (a default category is created instead)
    - Squashed all previous migrations

--- achievements/models.py
+++ achievements/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from membership_file.models import Member
+from django.contrib.auth.models import User
 
 #Setup some constants
 maxDescriptionLength = 255
@@ -8,22 +8,29 @@ maxNameLength = 31
 
 # Create categories for the Achievements like Boardgames, Roleplay, General
 class Category(models.Model):
+    class Meta:
+        # Enabled proper plurality
+        verbose_name_plural = "categories"
+
     name = models.CharField(max_length=maxNameLength)
     description = models.TextField(max_length=maxDescriptionLength)
 
     def __str__(self):
         return self.name
 
-# The Achievement model represents the achievements in the achievements file.
-class Achievement(models.Model):
-    # Different achievements can have the same category, but every Achievement can only have one category.
-    category = models.ForeignKey(Category, on_delete = models.SET_DEFAULT, default=1, blank = True, related_name = "related_achievements")
+# Gets or creates the default Category
+def get_or_create_default_category():  
+    return Category.objects.get_or_create(name='General', description='Contains Achievements that do not belong to any other Category.')[0]
 
+# Achievements that can be earned by users
+class Achievement(models.Model):
+    # Basic Information
     name = models.CharField(max_length=maxNameLength)
     description = models.TextField(max_length=maxDescriptionLength)
+    category = models.ForeignKey(Category, on_delete=models.SET(get_or_create_default_category), related_name="related_achievements")
 
     # An Achievement can be claimed by more members (claimants) and a member can have more achievements.
-    claimants = models.ManyToManyField(Member, blank = True, related_name = "claimed_achievements")
+    claimants = models.ManyToManyField(User, blank=True, related_name="claimed_achievements")
 
     def __str__(self):
         return self.name
--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Gets the display name of the user
+def get_display_name(self):
+    if self.first_name is not None:
+        return self.first_name
+    return self.username
+
+# Register the new method
+User.add_to_class("get_display_name", get_display_name)
