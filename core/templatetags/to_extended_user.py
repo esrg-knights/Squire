79e87649ce7f3586c1ba158bebc2f316cdfa99d9
EricTRL <34304046+EricTRL@users.noreply.github.com>
2020-03-21 17:32:42 +0100

    Issue #35: General Frontend: Base template and Homepage
    Modified the base template such that it...
    - ...now features its own colour-scheme
    - ...is aligned properly.
    - ...includes a brand icon
    - ...has a sensible navigation bar
    
    Updated the text on the homepage, and ensured that is is formatted nicely.
    
    Added a to_extended_user filter that transforms a User to an ExtendedUser.

---
+++ core/templatetags/to_extended_user.py
@@ -0,0 +1,16 @@
+from django import template
+from core.models import ExtendedUser
+
+register = template.Library()
+
+##################################################################################
+# Template Filter that transforms a User to an ExtendedUser
+# @since 12 MAR 2020
+##################################################################################
+
+@register.filter
+def to_extended_user(user):
+    attrs = {field.name: getattr(user, field.name) for field in user._meta.fields}
+    print(user)
+    print(ExtendedUser(**attrs).get_display_name())
+    return ExtendedUser(**attrs)
