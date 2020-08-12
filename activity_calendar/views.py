a888ae90b36bc74fb6f8abfae9f1ce49a2f881a6
EricTRL <34304046+EricTRL@users.noreply.github.com>
2020-08-12 17:45:53 +0200

    Issue #48: ical feed: FullCalendar DST + EXDATE bugfix
    Fixed a bug where the EXDATE of activities were not taken into account.
    
    FullCalendar now properly displays recurring events even if a DST-change occurs.

--- activity_calendar/views.py
+++ activity_calendar/views.py
@@ -13,30 +13,12 @@ from .models import Activity
 def googlehtml_activity_collection(request):
     return render(request, 'activity_calendar/calendar.html', {})
 
-
 # Renders the calendar page, which utilises FullCalendar
 @require_safe
 def activity_collection(request):
-    # all_events = Events.objects.all()
+    return render(request, 'activity_calendar/fullcalendar.html', {})
 
-    # if request.GET:  
-    #     event_arr = []
-
-    #     for i in all_events:
-    #         event_sub_arr = {}
-    #         event_sub_arr['title'] = i.event_name
-    #         start_date = datetime.datetime.strptime(str(i.start_date.date()), "%Y-%m-%d").strftime("%Y-%m-%d")
-    #         end_date = datetime.datetime.strptime(str(i.end_date.date()), "%Y-%m-%d").strftime("%Y-%m-%d")
-    #         event_sub_arr['start'] = start_date
-    #         event_sub_arr['end'] = end_date
-    #         event_arr.append(event_sub_arr)
-    #     return HttpResponse(json.dumps(event_arr))
-
-    context = {
-        # "events":all_events,
-    }
-    return render(request, 'activity_calendar/fullcalendar.html', context)
-
+# The view that is accessed by FullCalendar to retrieve events
 def get_activity_json(activity, start, end):
     return {
         'groupId': activity.id,
@@ -92,24 +74,31 @@ def fullcalendar_feed(request):
 
     for recurring_activity in all_recurring_activities:
         recurrences = recurring_activity.recurrences
-        # start_time = recurring_activity.start_date.time()
-        # end_time = recurring_activity.end_date.time()
+        event_start_time = recurring_activity.start_date.astimezone(timezone.get_current_timezone()).time()
+        utc_start_time = recurring_activity.start_date.time()
+
+        # recurrence expects each EXDATE's time to match the event's start time (in UTC; ignores DST)
+        # Why it doesn't store it that way in the first place remains a mystery
+        recurrences.exdates = list(map(lambda dt:
+                datetime.combine(timezone.localtime(dt).date(),
+                    utc_start_time, tzinfo=timezone.utc),
+                recurrences.exdates
+        ))
 
         # If the activity ends on a different day than it starts, this also needs to be the case for the occurrence
-        # days_diff = recurring_activity.end_date.date() - recurring_activity.start_date.date()
         time_diff = recurring_activity.end_date - recurring_activity.start_date
-        print(recurring_activity)
+
         for occurence in recurrences.between(start_date, end_date, dtstart=recurring_activity.start_date, inc=True):
-            print(occurence)
-            # occurence_start_date = datetime.combine(occurence.date(), start_time, tzinfo=timezone.utc)
+            # recurrence does not handle daylight-saving time! If we were to keep the occurence as is,
+            # then summer events would occur an hour earlier in winter!
+            occurence = timezone.get_current_timezone().localize(
+                datetime.combine(timezone.localtime(occurence).date(), event_start_time)
+            )
+
             activities.append(get_activity_json(
                 recurring_activity,
-                occurence.isoformat(),# occurence_start_date.isoformat(),
-                occurence + time_diff,# datetime.combine(occurence.date() + days_diff, end_time, tzinfo=timezone.utc).isoformat()
+                occurence.isoformat(),
+                (occurence + time_diff).isoformat(),
             ))
 
     return JsonResponse({'activities': activities})
-
-
-# https://stackoverflow.com/questions/8858426/fullcalendar-json-feed-caching
-# https://fullcalendar.io/docs/recurring-events
