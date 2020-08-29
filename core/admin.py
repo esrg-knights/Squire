87916450959ef7ad6cd09c1c2e981e54553b2072
EricTRL <34304046+EricTRL@users.noreply.github.com>
2020-08-29 14:16:29 +0200

    Issue #50: activity subscriptions: Admin Panel + Migrations squash
    Made the admin panel (for presetImages) more user-friendly.

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import PresetImage
 
-admin.site.register([PresetImage])
+class PresetImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'image', 'selectable')
+    list_filter = ['selectable']
+    list_display_links = ('id', 'name')
+
+admin.site.register(PresetImage, PresetImageAdmin)
