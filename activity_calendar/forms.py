07a1e2190b530b11fcf5de2c7b8e9554b23f744d
EricTRL <34304046+EricTRL@users.noreply.github.com>
2020-08-30 11:26:38 +0200

    Issue #50: activity subscriptions: Review changes
    Moved code around to a logical place

--- activity_calendar/forms.py
+++ activity_calendar/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext, gettext_lazy as _
+from django.core.validators import ValidationError
 
 from .models import ActivitySlot
 from core.models import PresetImage
@@ -19,14 +20,15 @@ class ActivitySlotForm(ModelForm):
         self.fields['image'].queryset = PresetImage.objects.filter(selectable=True)
         self.fields['image'].label_from_instance = lambda image: image.name
 
-    def is_valid(self):
-        ret = forms.Form.is_valid(self)
+    def clean_max_participants(self):
+        max_participants = self.cleaned_data['max_participants']
 
         # Users can only create slots for at least 1 participant
-        # NB: At this point the data is a string
         if self.data['max_participants'] == '0':
-            self.errors.update({'max_participants': ['A slot cannot have 0 participants']})
-            ret = False
+            raise ValidationError(['A slot cannot have 0 participants'])
+
+    def is_valid(self):
+        ret = forms.Form.is_valid(self)
 
         # Add an 'error' class to input elements that contain an error
         for field in self.errors:
