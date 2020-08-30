7a21c34db9cbc67dae6347ae6119735c27c510bc
EricTRL <34304046+EricTRL@users.noreply.github.com>
2020-08-30 16:27:26 +0200

    Achievement admin panel + COVID Rules
    Made the achievement model easier to work with on the admin side of things.
    
    Added the COVID rules on each activity's page.

--- achievements/admin.py
+++ achievements/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Category, Achievement, Claimant
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Achievement)
-admin.site.register(Claimant)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+
+admin.site.register(Category, CategoryAdmin)
+
+class ClaimantInline(admin.TabularInline):
+    model = Claimant
+    extra = 0
+
+class AchievementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'is_public')
+    list_filter = ['category', 'is_public']
+    list_display_links = ('id', 'name')
+
+    inlines = [ClaimantInline]
+
+admin.site.register(Achievement, AchievementAdmin)
+
