aedb694618cbbb5e5b2c483aa1c217ad94d8dd77
EricTRL <34304046+EricTRL@users.noreply.github.com>
2020-09-01 13:52:59 +0200

    Issue #57: non-recurring acitivyt signup: Register/deregister
    Can now register and deregister for slots of non-recurring activities.

--- activity_calendar/views.py
+++ activity_calendar/views.py
@@ -161,7 +161,12 @@ def register(request, slot_id):
        request.user, through_defaults={}
     )
 
-    q_str = urlencode({'date': slot.recurrence_id.isoformat()})
+    q_str = ''
+    if slot.parent_activity.is_recurring:
+        q_str = urlencode({'date': slot.recurrence_id.isoformat()})
+    else:
+        q_str = urlencode({'date': slot.parent_activity.start_date.isoformat()})
+
     return HttpResponseRedirect(
         f"{reverse('activity_calendar:activity_slots_on_day', kwargs={'activity_id': parent_activity.id})}?{q_str}")
 
@@ -182,7 +187,12 @@ def deregister(request, slot_id):
     for usr in user_slot_participants:
         slot.participants.remove(usr)
 
-    q_str = urlencode({'date': slot.recurrence_id.isoformat(), 'deregister': True})
+    q_str = ''
+    if slot.parent_activity.is_recurring:
+        q_str = urlencode({'date': slot.recurrence_id.isoformat(), 'deregister': True})
+    else:
+        q_str = urlencode({'date': slot.parent_activity.start_date.isoformat(), 'deregister': True})
+
     return HttpResponseRedirect(
         f"{reverse('activity_calendar:activity_slots_on_day', kwargs={'activity_id': parent_activity.id})}?{q_str}")
 
