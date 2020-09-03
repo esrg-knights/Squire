60bbd4c82ee4e0acf2cf265274a2929a19e393f2
EricTRL <34304046+EricTRL@users.noreply.github.com>
2020-09-03 19:35:36 +0200

    Issue #60: browser compatibility: MS Edge
    Removed occurences of rest parameters (= ...) from the Javascript code as MS Edge v44 did not support it, preventing the activity calendar from loading there.
    
    Added a missing closing div.
    
    Modified the activity calendar default scroll to even later in the evening, as the common evening activities are not visible without scrolling on small-screen devices.

--- activity_calendar/static/js/fullcalendar-init.js
+++ activity_calendar/static/js/fullcalendar-init.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         // Determines how far forward the scroll pane is initially scrolled.
         // This value ensures that evening-activities are visible without needing to scroll
-        scrollTime: '14:00:00',
+        scrollTime: '19:00:00',
         // customize the button names,
         // otherwise they'd all just say "list"
         views: {
@@ -86,17 +86,11 @@ function onEventClick(info, calendar) {
         }
     } else {
         // 5 augustus, 19:30 – 6 augustus, 02:00
-        var opts = {}
-
-        if (!event.allDay) {
-            opts = {hour: 'numeric', minute: 'numeric'}
-        }
-
         date_str = start_date.toLocaleString('en-gb', {
-            month: 'long', day: 'numeric', ...opts
+            month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric',
         })
         date_str += ' - ' + end_date.toLocaleString('en-gb', {
-            month: 'long', day: 'numeric', ...opts
+            month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric',
         })
     }
 
