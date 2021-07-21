73e543173557cccbc26009f3b0719df947401585
EricTRL <34304046+EricTRL@users.noreply.github.com>
2021-07-21 11:35:11 +0200

    Regenerated Migrations + New Permission
    Added a new permission for viewing private slot locations, even if not registered for that slot. This permission is not yet used anywhere.
    
    Regenerated all migrations of this branch due to migration conflicts.

--- activity_calendar/models.py
+++ activity_calendar/models.py
@@ -41,6 +41,7 @@ class Activity(models.Model):
             ('can_ignore_none_slot_creation_type',          "[F] Can create slots for activities with slots that normally do not allow slot creation by users."),
             ('can_ignore_slot_creation_limits',             "[F] Can create more slots even if the maximum amount of slots is reached."),
             ('can_select_slot_image',                       "[F] Can choose an alternative slot image when creating a slot."),
+            ('can_view_private_slot_locations',             "[F] Can view a slot's location even if they are marked as 'private' by the activity."),
         ]
 
 
