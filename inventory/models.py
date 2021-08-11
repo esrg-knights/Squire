fe1f862b5a75aaa6d58fbb883864cfe7021eafad
DutcherNL <wouter@fam-roos.com>
2021-08-11 19:36:46 +0200

    Implemented feedback on boardgame page

--- inventory/models.py
+++ inventory/models.py
@@ -120,11 +120,16 @@ class Item(models.Model):
         exclude_names = ('id', 'name', 'description', 'image')
         for field in self._meta.local_fields:
             if field.name not in exclude_names:
-                other_fields.append({
+                field_dict = {
                     'name': field.name,
                     'verbose_name': field.verbose_name,
                     'value': getattr(self, field.name),
-                })
+                }
+                # Set a value in case there are choices
+                if hasattr(self, f'get_{field.name}_display'):
+                    field_dict['display_value'] = getattr(self, f'get_{field.name}_display')()
+
+                other_fields.append(field_dict)
         return other_fields
 
 
@@ -189,3 +194,30 @@ class MiscellaneousItem(Item):
 class BoardGame(Item):
     """ Defines boardgames """
     bgg_id = models.IntegerField(blank=True, null=True)
+
+    player_min = models.PositiveIntegerField(blank=True, null=True)
+    player_max = models.PositiveIntegerField(blank=True, null=True)
+
+    play_duration_options = [
+        ('XS', '<15 min.'),
+        ('S', '< 1 hour'),
+        ('N', '1-2 hours'),
+        ('L', '3-4 hours'),
+        ('XL', '4+ hours'),
+    ]
+
+    play_duration = models.CharField(max_length=2, choices=play_duration_options, blank=True, null=True)
+
+    def get_players_display(self):
+        if self.player_min and not self.player_max:
+            return f'{self.player_min}+'
+        if self.player_min and self.player_max:
+            return f'{self.player_min} - {self.player_max}'
+        if not self.player_min and self.player_max:
+            return f'{self.player_max}-'
+        return ''
+
+    def clean(self):
+        if self.player_min and self.player_max:
+            if self.player_min > self.player_max:
+                raise ValidationError("The minimum player count can not be higher than the maximum player count", code='incorrect_value')
--- inventory/tests/test_models.py
+++ inventory/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.db import models
 from django.contrib.auth.models import Group
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
@@ -151,3 +152,35 @@ class TestItemManager(TestCase):
         self.assertEqual(0, self.manager.get_all_owned_by(group=group).count())
         group = Group.objects.get(id=2)
         self.assertEqual(2, self.manager.get_all_owned_by(group=group).count())
+
+
+class TestBoardGame(TestCase):
+
+    def test_duration_field(self):
+        # Test that play_duration is a choice field with 5 choices
+        field = BoardGame._meta.get_field("play_duration")
+        self.assertIsNotNone(field.choices)
+        self.assertEqual(len(field.choices), 5)
+
+    def test_get_players_display(self):
+        txt = BoardGame(name='test-game')
+        self.assertEqual(txt.get_players_display(), '')
+
+        txt = BoardGame(name='test-game', player_min=2)
+        self.assertEqual(txt.get_players_display(), '2+')
+
+        txt = BoardGame(name='test-game', player_max=9)
+        self.assertEqual(txt.get_players_display(), '9-')
+
+        txt = BoardGame(name='test-game', player_min=3, player_max=5)
+        self.assertEqual(txt.get_players_display(), '3 - 5')
+
+    def test_playter_clean(self):
+        try:
+            BoardGame(name='test-game', player_min=3, player_max=5).clean()
+        except ValidationError as error:
+            raise AssertionError("Error raised: "+str(error))
+
+        with self.assertRaises(ValidationError) as error:
+            BoardGame(name='test-game', player_min=5, player_max=3).clean()
+        self.assertEqual(error.exception.code, 'incorrect_value')
