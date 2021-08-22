43e6443a871154970f4c146b1afe5ada5726a1d0
DutcherNL <wouter@fam-roos.com>
2021-08-22 12:00:04 +0200

    Doubled dice max-length size

--- roleplaying/models.py
+++ roleplaying/models.py
@@ -47,7 +47,7 @@ class RoleplayingSystem(models.Model):
     rate_lore = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)],
                                      blank=True, null=True)
     player_count = models.CharField(max_length=8, null=True, blank=True)
-    dice = models.TextField(max_length=32, null=True, blank=True)
+    dice = models.TextField(max_length=64, null=True, blank=True)
 
     # Relations
     achievements = GenericRelation('achievements.AchievementItemLink')
