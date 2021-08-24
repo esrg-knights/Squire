72f2b9f315a82e2399b70d6972b3af478f7f5913
DutcherNL <wouter@fam-roos.com>
2021-08-24 17:11:55 +0200

    Improved associationgroup admin

--- committees/admin.py
+++ committees/admin.py
@@ -9,10 +9,11 @@ class MembershipInline(admin.TabularInline):
     min_num = 0
 
 @admin.register(AssociationGroup)
-class AchievementAdmin(admin.ModelAdmin):
+class AssociationGroupAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'shorthand',)
     list_filter = ['type', 'is_public']
     list_display_links = ('id', 'name')
+    fields = [('site_group', 'shorthand'), ('type', 'is_public'), 'icon', 'short_description', 'long_description']
 
     inlines = [MembershipInline]
 
