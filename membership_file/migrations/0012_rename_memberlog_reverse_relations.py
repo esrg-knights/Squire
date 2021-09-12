66796dca3f5359fa812ecc0a079539087f0114ec
EricTRL <34304046+EricTRL@users.noreply.github.com>
2021-09-12 18:47:39 +0200

    Fix conflicting migration

--- membership_file/migrations/0012_rename_memberlog_reverse_relations.py
+++ membership_file/migrations/0012_rename_memberlog_reverse_relations.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('membership_file', '0010_export_permission'),
+        ('membership_file', '0011_delete_memberuser'),
     ]
 
     operations = [
