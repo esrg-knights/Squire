a36e88f28c566fe12f0c8b7ded65fd3a76be78eb
Ndoll1 <nickydoll@home.nl>
2021-11-23 00:22:09 +0100

    Some code clean-up and translation fixes

--- user_interaction/static/themes/kinjin-theme.js
+++ user_interaction/static/themes/kinjin-theme.js
@@ -2,18 +2,24 @@
 $( document ).ready(function() {
     console.log('hello!')
     html = $('body main').html()
-    // Replace 'k' and 'c' by 'q' (leave html between < and > alone)
+    // Replace X with Y
  
+  html = html.replace('Squire', 'スクワイア')
+  html = html.replace('squire', 'スクワイア')
+
   html = html.replace('board games', 'ボードゲーム')
   html = html.replace('boardgames', 'ボードゲーム')
   html = html.replace('Board games', 'ボードゲーム')
   html = html.replace('Boardgames', 'ボードゲーム')
-   html = html.replace('board game', 'ボードゲーム')
-   html = html.replace('boardgame', 'ボードゲーム')
+  html = html.replace('board game', 'ボードゲーム')
+  html = html.replace('boardgame', 'ボードゲーム')
+  html = html.replace('boardgame', 'ボードゲーム') //werkt blijkbaar maar 1 instance, maar vaak tenminste 2x nodig, nog fixen. Daarom 2x dit
+  
   html = html.replace('Activities in the next 7 days', '今後7日間の活動')
 
   html = html.replace('board', 'Student Council')
   html = html.replace('Board', 'Student Council')
+  html = html.replace('Boards', 'Student Council')
 
   html = html.replace('annoying', 'kawaii')
   html = html.replace('troops', 'oniisan')
@@ -22,10 +28,12 @@ $( document ).ready(function() {
   html = html.replace('Report Bug', 'バグの報告')
   html = html.replace('Hello','こんにちは')
   html = html.replace('Achievements', '実績')
-  html = html.replace('Activities', '活動')
+  html = html.replace('Activities', 'アクティビティ')
   html = html.replace('Boardgames', 'ボードゲーム')
+  html = html.replace('Roleplaying systems','ロールプレイシステム')
   html = html.replace('Roleplay', 'ロールプレイ')
   html = html.replace('Committees', '委員会')
+  html - html.replace('Order','オーダー')
   html = html.replace('Account', 'アカウント')
 
   html = html.replace('Monday', 'げつようび')
@@ -49,16 +57,22 @@ $( document ).ready(function() {
   html = html.replace('November', '十一月')
   html = html.replace('December', '十二月')
 
-  html = html.replace('Sign-ups are closed', 'サインアップは締め切られました')
-  html = html.replace('Open for sign-ups', 'サインアップのために開いています')
-  html = html.replace('You are subscribed', 'あなたは購読しています')
+  html = html.replace('00:00', '24:00')
+  html = html.replace('01:00', '25:00')
+  html = html.replace('02:00', '26:00')
+  html = html.replace('03:00', '27:00')
+  html = html.replace('04:00', '28:00')
+
+  html = html.replace('Sign-ups are closed', 'サインアップクローズ')
+  html = html.replace('Open for sign-ups', 'サインアップオープン')
+  html = html.replace('You are subscribed', 'サブスクライブ')
 
   html = html.replace('Go to activity', 'アクティビティに移動')
   html = html.replace('Register', '書き記す')
   html - html.replace('Create Slot', 'スロットを作成する')
-  html = html.replace('Registrations are required! You cannot join this activity otherwise!', '登録が必要です！それ以外の場合は、このアクティビティに参加できません。')
 
-    $('body main').html(html)
+  $('body main').html(html)
+  
 })
 // function googleTranslateElementInit() {
 //   new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
