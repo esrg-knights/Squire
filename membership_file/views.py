40297febb9e66b2ab5c6f6022c10a4cada15d7fa
DutcherNL <wouter@fam-roos.com>
2021-12-22 14:05:17 +0100

    Added message (and easteregg) when extending already extended year

--- membership_file/views.py
+++ membership_file/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.core.exceptions import PermissionDenied
 from django.views.generic import TemplateView, FormView
+from django.template.response import TemplateResponse
 from django.urls import reverse_lazy
 from dynamic_preferences.registries import global_preferences_registry
 
@@ -10,6 +11,7 @@ from .util import MembershipRequiredMixin
 from .auto_model_update import *
 from .export import *
 from membership_file.forms import ContinueMembershipForm
+from membership_file.models import Membership
 
 global_preferences = global_preferences_registry.manager()
 
@@ -38,12 +40,30 @@ class UpdateMemberYearMixin:
 
         super(UpdateMemberYearMixin, self).setup(request, *args, **kwargs)
 
+    def get_context_data(self, **kwargs):
+        context = super(UpdateMemberYearMixin, self).get_context_data(**kwargs)
+        context['new_year'] = self.year
+        return context
+
 
 
 class ExtendMembershipView(UpdateMemberYearMixin, FormView):
     template_name = "membership_file/extend_membership.html"
     form_class = ContinueMembershipForm
     success_url = reverse_lazy('membership_file/continue_success')
+
+    def dispatch(self, request, *args, **kwargs):
+        if Membership.objects.filter(
+            year=self.year,
+            member=self.request.member
+        ).exists():
+            return TemplateResponse(
+                request,
+                template="membership_file/extend_membership_already_done.html",
+                context=self.get_context_data()
+            )
+
+        return super(ExtendMembershipView, self).dispatch(request, *args, **kwargs)
 
     def get_form_kwargs(self):
         kwargs = super(ExtendMembershipView, self).get_form_kwargs()
