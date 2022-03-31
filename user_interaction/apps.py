d913ca61b739a09f8425f0bf0c502e7c62f20b8a
EricTRL <34304046+EricTRL@users.noreply.github.com>
2022-03-31 17:04:07 +0200

    April Fools 2022
    Renames members in the frontend (does not affect the database itself), and adds an "upgrade" page.

--- user_interaction/apps.py
+++ user_interaction/apps.py
@@ -6,6 +6,7 @@ class UserInteractionConfig(AppConfig):
 
     def ready(self):
         from membership_file.util import get_member_from_user
+        from dynamic_preferences.registries import global_preferences_registry
 
         def _get_user_display_name(user):
             """
@@ -19,6 +20,41 @@ class UserInteractionConfig(AppConfig):
                 - Real name set: <first_name>
             """
             member = get_member_from_user(user)
+
+            ################
+            # BEGIN APRIL 2022
+            ################
+            # Rename everyone to Dennis/Laura, and rename the actual ones to Sjors
+            global_preferences = global_preferences_registry.manager()
+            if global_preferences['homepage__april_2022']:
+                namelist = []
+                # Members
+                if member is not None:
+                    if member.first_name in ["Laura", "Dennis", "Denise"]:
+                        namelist.append("Sjors")
+                    elif len(member.first_name) % 2 == 0:
+                        namelist.append("Dennis")
+                    else:
+                        namelist.append("Laura")
+
+                    if member.tussenvoegsel:
+                        namelist.append(member.tussenvoegsel)
+
+                    namelist.append(member.last_name)
+                else:
+                    # Non-members
+                    namelist = (user.first_name or user.username).split()
+                    if namelist[0] in ["Laura", "Dennis", "Denise"]:
+                        namelist[0] = "Sjors"
+                    elif len(namelist[0]) % 2 == 0:
+                        namelist[0] = "Dennis"
+                    else:
+                        namelist[0] = "Laura"
+                return " ".join(namelist)
+            ################
+            # END APRIL 2022
+            ################
+
             if member is not None:
                 return member.get_full_name()
             return (user.first_name or user.username)
