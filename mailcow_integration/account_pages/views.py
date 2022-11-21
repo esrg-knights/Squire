1b53bcf306cf96aa27e9f89dac618b3bd2dd2f99
Eric <34304046+EricTRL@users.noreply.github.com>
2022-11-21 11:24:16 +0100

    Moved preference label-formatting to preference
    Label-formatting is now already done at the preference itself, rather than its form.

--- mailcow_integration/account_pages/views.py
+++ mailcow_integration/account_pages/views.py
@@ -1,14 +1,12 @@
-from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth.views import PasswordChangeView
-from django.views.generic import TemplateView, FormView, UpdateView
 from django.urls import reverse_lazy
-from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
-from dynamic_preferences.users.forms import user_preference_form_builder
+from django.views.generic import FormView
 from dynamic_preferences.forms import PreferenceForm
+from dynamic_preferences.users.forms import user_preference_form_builder
 
 from user_interaction.accountcollective import AccountViewMixin
+
 
 class EmailPreferencesChangeView(AccountViewMixin, FormView):
     """ View for updating mail preferences """
@@ -17,33 +15,6 @@ class EmailPreferencesChangeView(AccountViewMixin, FormView):
 
     def get_form_class(self):
         return user_preference_form_builder(instance=self.request.user, section='mail')
-
-    def get_form(self, form_class=None):
-        form = super().get_form(form_class)
-
-        # Add additional info to labels and/or disable options based on config
-        member_aliases = settings.MEMBER_ALIASES
-        for boundfield in form:
-            # Find relevant config
-            alias_id = boundfield.auto_id.split("__")[1]
-            alias_config = member_aliases[alias_id]
-            label_elements = ["{}"]
-
-            # Disable form field if opt-outs are disallowed
-            if not alias_config['allow_opt_out']:
-                boundfield.field.disabled = True
-                label_elements.append('<span class="badge badge-warning badge-pill"><i class="fas fa-lock"></i> Cannot opt-out</span>')
-
-            # Explicitly mark non-internal aliases as "public"
-            if not alias_config['internal']:
-                label_elements.append('<span class="badge badge-primary badge-pill"><i class="fas fas fa-globe"></i> Public</span>')
-
-            # Properly escape HTML
-            boundfield.label = format_html(
-                ' '.join(label_elements),
-                boundfield.label
-            )
-        return form
 
     def form_valid(self, form: PreferenceForm):
         message = _("Your mail preferences have been updated!")
--- mailcow_integration/dynamic_preferences_registry.py
+++ mailcow_integration/dynamic_preferences_registry.py
@@ -1,9 +1,7 @@
-import re
-from typing import Optional
-from django.apps import apps
 from django.conf import settings
-from dynamic_preferences.types import BooleanPreference
+from django.utils.html import format_html
 from dynamic_preferences.preferences import Section
+from dynamic_preferences.types import BooleanPreference
 from dynamic_preferences.users.registries import user_preferences_registry
 
 mail = Section('mail')
@@ -14,10 +12,33 @@ for alias_id, alias_data in settings.MEMBER_ALIASES.items():
     @user_preferences_registry.register
     class MemberEmailPreference(BooleanPreference):
         """ Theming of the application """
+
+        def _get_label() -> str:
+            """ Gets additional text data to include in the preference's name """
+            label_elements = ["{}"]
+
+            # Disable form field if opt-outs are disallowed
+            if not alias_data['allow_opt_out']:
+                label_elements.append('<span class="badge badge-warning badge-pill"><i class="fas fa-lock"></i> Cannot opt-out</span>')
+
+            # Explicitly mark non-internal aliases as "public"
+            if not alias_data['internal']:
+                label_elements.append('<span class="badge badge-primary badge-pill"><i class="fas fas fa-globe"></i> Public</span>')
+
+            # Properly escape HTML
+            return format_html(
+                ' '.join(label_elements),
+                alias_data['description'],
+            )
+
         section = mail
         name = alias_id
-        default = True
-        verbose_name = alias_data['description']
+        default = alias_data['default_opt']
+        verbose_name = _get_label()
         description = f"Subscribed to {alias_data['address']}"
         # help_text = ''
+
+        field_kwargs = {
+            'disabled': not alias_data['allow_opt_out'],
+        }
 
