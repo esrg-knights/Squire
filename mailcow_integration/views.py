65023a5ff34214d2bed5a946bf8805013e008038
Eric <34304046+EricTRL@users.noreply.github.com>
2022-11-21 16:19:26 +0100

    Update member preferences on status page
    Added a button that updates all member preferences.
    
    Member aliases are now finished, with the exception of proper error handling and additional verification (e.g. users setting a member-alias as their own email)

--- mailcow_integration/views.py
+++ mailcow_integration/views.py
@@ -3,6 +3,8 @@ from typing import Dict, List, Optional
 
 from django.apps import apps
 from django.conf import settings
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView, FormView
 
 from mailcow_integration.api.exceptions import MailcowAPIAccessDenied, MailcowAPIReadWriteAccessDenied, MailcowAuthException, MailcowException
@@ -29,20 +31,20 @@ class MailcowStatusView(SuperUserRequiredMixin, TemplateView):
         config = apps.get_app_config("mailcow_integration")
         self.mailcow_client: Optional[SquireMailcowManager] = config.mailcow_client
 
-    # TODO: Placeholder; move elsewhere
-    def _get_subscribed_member_addresses(self, alias_id: str) -> List[str]:
-        """ TODO """
-        # TODO: Support opt-outs for specific lists
-        return list(Member.objects.filter_active().order_by('email').values_list('email', flat=True))
-
     def _verify_aliasses_exist(self, alias_addresses: Dict[str, Dict], all_aliases: List[MailcowAlias], public_comment: str) -> Dict[str, Dict]:
         """ TODO """
+        self.mailcow_client: SquireMailcowManager
+        active_members = self.mailcow_client.get_active_members()
         results = {
-            alias_id: {
+            alias_config['address']: {
                 'alias': None,
                 'status': AliasStatus.MISSING.name,
-                'squire_subscribers': self._get_subscribed_member_addresses(alias_id),
-                'address': alias_config['address'],
+                'squire_subscribers': self.mailcow_client.get_subscribed_members(
+                    active_members,
+                    alias_id,
+                    default=alias_config['default_opt']
+                ),
+                'id': alias_id,
                 'description': alias_config['description'],
                 'internal': alias_config['internal'],
                 'allow_opt_out': alias_config['allow_opt_out'],
@@ -51,11 +53,12 @@ class MailcowStatusView(SuperUserRequiredMixin, TemplateView):
         }
 
         for alias in all_aliases:
-            if alias.address in alias_addresses:
+            if alias.address in results.keys():
                 if alias.public_comment != public_comment:
                     results[alias.address].update({'alias': alias, 'status': AliasStatus.NOT_MANAGED_BY_SQUIRE.name})
-                elif alias.goto != results[alias.address]['squire_subscribers']:
+                elif alias.goto != list(results[alias.address]['squire_subscribers'].values_list('email', flat=True)):
                     # Note: Lists are sorted
+                    # TODO: verify internal-status
                     results[alias.address].update({'alias': alias, 'status': AliasStatus.OUTDATED.name})
                 else:
                     results[alias.address].update({'alias': alias, 'status': AliasStatus.VALID.name})
@@ -85,6 +88,17 @@ class MailcowStatusView(SuperUserRequiredMixin, TemplateView):
 
                 # TODO: committee-aliases
 
-                # TODO: List "Managed by Squire" aliases that are no longer in use (to allow cleanup)
+                # TODO: List "[Managed by Squire]" aliases that are no longer in use (to allow cleanup)
 
         return context
+
+    def post(self, request, *args, **kwargs):
+        if self.request.POST.get("alias_type", None) == "members":
+            self.mailcow_client.update_member_aliases()
+            messages.success(self.request, "Member aliases updated.")
+        else:
+            # TODO: Update user aliases
+            messages.success(self.request, "Committee aliases updated.")
+            pass
+
+        return HttpResponseRedirect(self.request.get_full_path())
