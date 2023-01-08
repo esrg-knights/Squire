46743a0ae522099ea4ce13b9f17f60068b260411
DutcherNL <23361463+DutcherNL@users.noreply.github.com>
2023-01-08 17:14:21 +0100

    Removed no longer used membership_required_decorator (everything is class based views now)

--- membership_file/util.py
+++ membership_file/util.py
@@ -21,31 +21,6 @@ def get_member_from_user(user):
     return None
 
 
-def membership_required(function=None, fail_url=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
-    """
-    Decorator for views that checks that the user is a member, redirecting
-    to a special page if necessary.
-    Automatically calls Django's login_required
-    Based on Django's login_required and user_passes_test decorators
-    """
-    def decorator(view_func):
-        @wraps(view_func)
-        def _wrapped_view(request, *args, **kwargs):
-
-            # If the user is authenticated and a member with the same userID exists, continue
-            if request.member and request.member.is_considered_member():
-                return view_func(request, *args, **kwargs)
-
-            # Otherwise show the "Not a member" error page
-            resolved_fail_url = resolve_url(fail_url or settings.MEMBERSHIP_FAIL_URL)
-            return HttpResponseRedirect(resolved_fail_url)
-        # Wrap inside the login_required decorator (as non-logged in users can never be members)
-        return login_required(_wrapped_view, login_url=login_url, redirect_field_name=redirect_field_name)
-
-    if function:
-        return decorator(function)
-    return decorator
-
 class MembershipRequiredMixin(LoginRequiredMixin):
     """
         Verifies that the current user is a member, redirecting to a special page if needed.
