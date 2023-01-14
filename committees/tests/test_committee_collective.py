b45fb1ddfcda379a53ae6b4471396d97387b7dad
DutcherNL <23361463+DutcherNL@users.noreply.github.com>
2023-01-14 19:57:38 +0100

    A little more testcases, so the file is 100% codecovered

--- committees/tests/test_committee_collective.py
+++ committees/tests/test_committee_collective.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.models import User
-from django.test import TestCase, Client
+from django.contrib.auth.models import User, AnonymousUser
+from django.test import TestCase, Client, RequestFactory
 from unittest.mock import patch
 
 from committees.committeecollective import CommitteeBaseConfig, registry
@@ -10,8 +10,9 @@ class CommitteeConfigTestCase(TestCase):
     fixtures = ['test_users', 'test_members', 'test_groups', 'committees/associationgroups']
 
     def setUp(self):
+        self.user = User.objects.get(id=100)
         self.client = Client()
-        self.client.force_login(User.objects.get(id=100))
+        self.client.force_login(self.user)
         self.request = self.client.get('').wsgi_request
 
         self.patcher = patch('committees.committeecollective.user_in_association_group')
@@ -24,14 +25,18 @@ class CommitteeConfigTestCase(TestCase):
     def tearDown(self):
         self.patcher.stop()
 
-    def test_user_not_in_group(self):
+    def test_check_access_super(self):
+        # New request with no info WILL fail at the parent
+        request = RequestFactory().get(path='')
+        request.user = AnonymousUser()
+        self.assertEqual(self.config.check_access_validity(request, self.association_group), False)
+
+    def test_check_access_user_not_in_group(self):
         self.mock_user_in_a_group.return_value = False
         self.assertEqual(self.config.check_access_validity(self.request, self.association_group), False)
 
-    def test_is_default_for_group(self):
+    def test_check_access_no_permissions_set(self):
         self.assertEqual(self.config.check_access_validity(self.request, self.association_group), True)
-        self.config.group_requires_permission = 'auth.change_user'
-        self.assertEqual(self.config.check_access_validity(self.request, self.association_group), False)
 
     def test_check_access_without_permission_requirement(self):
         self.assertEqual(self.config.check_access_validity(self.request, self.association_group), True)
@@ -44,3 +49,22 @@ class CommitteeConfigTestCase(TestCase):
         self.config.group_requires_permission = 'auth.change_user'
         self.config.enable_access(self.association_group)
         self.assertEqual(self.config.check_access_validity(self.request, self.association_group), True)
+
+    def test_check_inexisting_perm(self):
+        with self.assertRaises(KeyError) as error:
+            self.config.group_requires_permission = 'xxx.yyy'
+            self.config.check_access_validity(self.request, self.association_group)
+        self.assertGreater(str(error.exception).find('xxx.yyy'), -1)
+
+    def test_enable_access(self):
+        perm_name = 'auth.change_user'
+        self.config.group_requires_permission = perm_name
+        self.config.enable_access(self.association_group)
+        self.assertEqual(self.user.has_perm(perm_name), True)
+
+    def test_disable_access(self):
+        perm_name = 'auth.change_user'
+        self.config.group_requires_permission = perm_name
+        self.config.enable_access(self.association_group)  # Assumes that enable_access works
+        self.config.disable_access(self.association_group)
+        self.assertEqual(self.user.has_perm(perm_name), False)
