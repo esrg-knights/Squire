e27f8eaeb39896b34bd2e45e028f181b331c1924
Eric <34304046+EricTRL@users.noreply.github.com>
2023-04-08 12:21:16 +0200

    Mail preference creation test

--- mailcow_integration/dynamic_preferences_registry.py
+++ mailcow_integration/dynamic_preferences_registry.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from django.conf import settings
 from django.utils.text import slugify
 from django.utils.html import format_html
@@ -12,37 +13,40 @@ def alias_address_to_id(address: str) -> str:
     """ Converts an alias address to a string compatible with django-dynamic-preferences """
     return slugify(address)
 
-for alias_address, alias_data in settings.MEMBER_ALIASES.items():
-    # Register user choice for each alias
-    @user_preferences_registry.register
-    class MemberEmailPreference(BooleanPreference):
-        """ Theming of the application """
+def register_preferences(member_aliases: Dict[str, Dict]):
+    """ Registers a user preference for each given member alias. """
+    for alias_address, alias_data in member_aliases.items():
+        # Register user choice for each alias
+        @user_preferences_registry.register
+        class MemberEmailPreference(BooleanPreference):
+            """ Email preferences """
+            def _get_label() -> str:
+                """ Gets additional text data to include in the preference's name """
+                label_elements = ["{}"]
 
-        def _get_label() -> str:
-            """ Gets additional text data to include in the preference's name """
-            label_elements = ["{}"]
+                # Disable form field if opt-outs are disallowed
+                if not alias_data['allow_opt_out']:
+                    label_elements.append('<span class="badge badge-warning badge-pill"><i class="fas fa-lock"></i> Cannot opt-out</span>')
 
-            # Disable form field if opt-outs are disallowed
-            if not alias_data['allow_opt_out']:
-                label_elements.append('<span class="badge badge-warning badge-pill"><i class="fas fa-lock"></i> Cannot opt-out</span>')
+                # Explicitly mark non-internal aliases as "public"
+                if not alias_data['internal']:
+                    label_elements.append('<span class="badge badge-primary badge-pill"><i class="fas fas fa-globe"></i> Public</span>')
 
-            # Explicitly mark non-internal aliases as "public"
-            if not alias_data['internal']:
-                label_elements.append('<span class="badge badge-primary badge-pill"><i class="fas fas fa-globe"></i> Public</span>')
+                # Properly escape HTML
+                return format_html(
+                    ' '.join(label_elements),
+                    alias_data['description'],
+                )
 
-            # Properly escape HTML
-            return format_html(
-                ' '.join(label_elements),
-                alias_data['description'],
-            )
+            section = mail
+            name = alias_address_to_id(alias_address)
+            default = alias_data['default_opt']
+            verbose_name = _get_label()
+            description = f"Subscribed to {alias_address}"
+            # help_text = ''
 
-        section = mail
-        name = alias_address_to_id(alias_address)
-        default = alias_data['default_opt']
-        verbose_name = _get_label()
-        description = f"Subscribed to {alias_address}"
-        # help_text = ''
+            field_kwargs = {
+                'disabled': not alias_data['allow_opt_out'],
+            }
 
-        field_kwargs = {
-            'disabled': not alias_data['allow_opt_out'],
-        }
+register_preferences(settings.MEMBER_ALIASES)
