e5e9507aef8a3db930a5a72c1a3cadb13e5930d2
LenaWil <lena@wildervanck.eu>
2025-10-08 01:24:08 +0200

    Correct the Ivoren Wachters thing

--- user_interaction/views.py
+++ user_interaction/views.py
@@ -80,7 +80,7 @@ welcome_messages = [
     "This message is sponsored by Fantasy Court",
     "Fun fact: The boardgame committee's favourite boardgame is Fortnite Monopoly!",
     "Fun fact: The roleplaying committee's favourite system is the Rolepearing Game!",
-    "Fun fact: The Ivoren Wachter's favourite swordfighting-move is the shield bash!",
+    "Fun fact: The Ivoren Wachters’ favourite swordfighting-move is the shield bash!",
     "Fun fact: The cooking committee's favourite dish is Soep Ultiem!",
     "Fun fact: Fantasy Court's favourite vendor is Conferences!",
     "Fun fact: The activity committee's favourite activity is drinking away sorrows in Hubble!",
